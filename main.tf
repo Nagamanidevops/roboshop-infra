6af61bcd5c74c7b4eb28a2b553c1e5e2a6265bb4
nagamani <nagamanichavaaws@gmail.com>
2023-02-21 09:35:09 +0000

    com

--- main.tf
+++ main.tf
@@ -1,6 +1,4 @@
+
 module "network" {
-  source = "https://github.com/Nagamanidevops/tf-module-vpc"
-  
-for_each = var.vpc
-cidr_block = each.value.cidr_block
-}
+  source  = "github.com/Nagamanidevops/tf-module-vpc"
+ }
