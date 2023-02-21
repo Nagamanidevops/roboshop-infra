14800ff20d151d15617061714ff844d77823707b
nagamani <nagamanichavaaws@gmail.com>
2023-02-21 07:07:11 +0000

    12

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 module "network" {
   source = "https://github.com/Nagamanidevops/tf-module-vpc"
+  version = "main"
   
-  for_each = var.vpc
-  cidr_block = each.value.cidr_block
+#for_each = var.vpc
+ # cidr_block = each.value.cidr_block
 }
