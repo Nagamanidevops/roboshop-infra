682f85a540a930accbf2e1ff88b2dc6bd3afdbc6
nagamani <nagamanichavaaws@gmail.com>
2023-02-22 06:43:20 +0000

    tags

--- env-dev/main.tfvars
+++
@@ -1,7 +0,0 @@
-env = "dev"
-
-vpc = {
-  main = {
-    cidr_block = "10.0.0.0/16"
-  }
-}
--- main.tf
+++ main.tf
@@ -1,6 +1,8 @@
 
 module "network" {
   source  = "github.com/Nagamanidevops/tf-module-vpc"
+  
+  env = var.env
   for_each = var.vpc
   cidr_block = each.value.cidr_block
  }
