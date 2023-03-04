7f6bac6f66e9266d5f404046c34baa7feded4c15
nagamani <nagamanichavaaws@gmail.com>
2023-03-04 05:39:52 +0000

    a

--- vars.tf
+++ vars.tf
@@ -1,10 +1,5 @@
-# variable "env"{}
-# variable "vpc"{}
-# variable "default_vpc_id"{}
+variable "env"{}
+variable "vpc"{}
+variable "default_vpc_id"{}
 
 
-variable "cidr_block" {}
-variable "env" {}
-variable "default_vpc_id" {}
-variable "subnets" {}
-variable "availability_zone" {}
