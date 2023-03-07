397141c248a170dda6b8615ace93d4b2223092dd
nagamani <nagamanichavaaws@gmail.com>
2023-03-07 06:13:31 +0000

    a

--- main.tf
+++ main.tf
@@ -20,8 +20,10 @@ module docdb {
   
   for_each = var.docdb
  
-  subnet_ids = lookup(lookup( lookup(lookup( module.vpc , each.value.vpc_name,null),  "private_subnets_ids",null),each.value.subnet_name,null), "subnet_ids"
+ // subnet_ids = lookup(lookup( lookup(lookup( module.vpc , each.value.vpc_name,null),  "private_subnets_ids",null),each.value.subnet_name,null), "subnet_ids"
 ,null)  
+  subnet_ids     = lookup(lookup(lookup(lookup(module.vpc, each.value.vpc_name, null), "private_subnet_ids", null), each.value.subnets_name, null), "subnet_ids", null)
+
 }
 
 output "vpc" {
